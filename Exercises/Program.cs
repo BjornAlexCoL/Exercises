b4fee61235f04ee2d689efc03b12a5cabaca5a2a
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2022-02-08 07:12:21 +0100

    Add project files.

---
+++ Exercises/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var keepAlive = true;
+            while (keepAlive)
+            {
+                try
+                {
+                    Console.Write("Enter assignment number (or -1 to exit): ");
+                    var assignmentChoice = int.Parse(Console.ReadLine() ?? "");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    switch (assignmentChoice)
+                    {
+                        case 1:
+                            RunExerciseOne();
+                            break;
+                        case 2:
+                            RunExerciseTwo();
+                            break;
+                        case -1:
+                            keepAlive=false;
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("That is not a valid exercise number!");
+                            break;
+                    }
+                    Console.ResetColor();
+                    Console.WriteLine("Hit any key to continue!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("That is not a valid exercise number!");
+                    Console.ResetColor();
+                }
+            }
+        }
+        private static void RunExerciseOne()
+        {
+            Console.WriteLine("You successfully ran exercise one!");
+        }
+        private static void RunExerciseTwo()
+        {
+            Console.WriteLine("You successfully ran exercise Two!");
+        }
+    }
+}
